9a5e00fcd3cc2546575169850eea4d8c23d8f166
Rando Õispuu <rando@veebisepad.ee>
2025-10-07 20:04:44 +0300

    Add .config/nvim/lua/plugins/blink.lua

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      { "adalessa/laravel.nvim" },
+    },
+    specs = {
+      { "Saghen/blink.compat", version = "*", lazy = true, opts = {} },
+    },
+    opts = {
+      keymap = {
+        preset = "enter",
+      },
+      sources = {
+        -- remember to enable your providers here
+        default = { "laravel", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          laravel = {
+            name = "laravel",
+            module = "blink.compat.source",
+            score_offset = 95, -- show at a higher priority than lsp
+          },
+        },
+      },
+    },
+  },
+}
