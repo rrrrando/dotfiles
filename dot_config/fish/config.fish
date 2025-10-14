952b4900ee6442d8807f532953fea6718531fb52
Rando Õispuu <rando@veebisepad.ee>
2025-10-14 20:33:55 +0300

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -21,7 +21,6 @@ set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 
 
-carapace _carapace | source
 starship init fish | source
 zoxide init fish | source
 
