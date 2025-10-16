f7a6f76d309be0b618d1ce3db6e1045d6cee3b0b
Rando Õispuu <rando@veebisepad.ee>
2025-10-16 09:46:35 +0300

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,17 +4,18 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
-  { import = "astrocommunity.motion.mini-surround" },
-  { import = "astrocommunity.motion.mini-ai" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.blade" },
-  { import = "astrocommunity.bars-and-lines.wakastat" },
-  { import = "astrocommunity.color.twilight-nvim" },
-  { import = "astrocommunity.recipes.picker-lsp-mappings" },
-  { import = "astrocommunity.git.octo-nvim" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+	{ import = "astrocommunity.motion.mini-surround" },
+	{ import = "astrocommunity.motion.mini-ai" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.blade" },
+	{ import = "astrocommunity.bars-and-lines.wakastat" },
+	{ import = "astrocommunity.color.twilight-nvim" },
+	{ import = "astrocommunity.recipes.picker-lsp-mappings" },
+	{ import = "astrocommunity.git.octo-nvim" },
+	{ import = "astrocommunity.ai.opencode-nvim" },
+	-- import/override with your plugins folder
 }
