b9f0f823c3323f0f5c078d7b00df80359439e7aa
Rando Õispuu <rando@veebisepad.ee>
2025-10-23 21:47:17 +0300

    Update .config/nvim/lua/plugins/neo-tree.lua

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -12,10 +12,11 @@ return {
         visible = true,
         show_hidden_count = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
+        hide_gitignored = false,
         hide_by_name = {
           ".git",
           ".DS_Store",
+          "node_modules",
         },
         never_show = {},
         never_show_by_pattern = { -- uses glob style patterns
