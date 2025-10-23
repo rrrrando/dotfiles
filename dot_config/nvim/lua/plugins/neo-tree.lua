98586e74c373b9d8e5033e58898e51fd76b342bc
Rando Õispuu <rando@veebisepad.ee>
2025-10-23 21:44:16 +0300

    Update .config/nvim/lua/plugins/neo-tree.lua

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,22 @@ return {
   opts = {
     hide_root_node = true,
     retain_hidden_root_indent = true,
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+        },
+        never_show = {},
+        never_show_by_pattern = { -- uses glob style patterns
+          ".null-ls_*",
+        },
+      },
+    },
     default_component_configs = {
       indent = {
         with_expanders = true,
@@ -17,23 +33,6 @@ return {
     window = {
       position = "left",
       width = 60,
-    },
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          -- '.git',
-          -- 'thumbs.db',
-          ".DS_Store",
-        },
-        never_show = {},
-        never_show_by_pattern = { -- uses glob style patterns
-          ".null-ls_*",
-        },
-      },
     },
   },
   config = function(_, opts)
