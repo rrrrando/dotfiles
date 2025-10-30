8675068b623baf65eae89eb583bab2f3de72c553
Rando Õispuu <rando@veebisepad.ee>
2025-10-30 11:00:22 +0200

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -19,9 +19,48 @@ return {
       filetypes = tsserver_filetypes,
     }
 
+    local vue_ls_config = {
+      on_init = function(client)
+        client.handlers["tsserver/request"] = function(_, result, context)
+          local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "ts_ls" })
+          local clients = {}
+
+          vim.list_extend(clients, ts_clients)
+
+          if #clients == 0 then
+            vim.notify(
+              "Could not find `ts_ls` lsp client, `vue_ls` would not work without it.",
+              vim.log.levels.ERROR
+            )
+            return
+          end
+          local ts_client = clients[1]
+
+          local param = unpack(result)
+          local id, command, payload = unpack(param)
+          ts_client:exec_cmd({
+            title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+            command = "typescript.tsserverRequest",
+            arguments = {
+              command,
+              payload,
+            },
+          }, { bufnr = context.bufnr }, function(_, r)
+            local response = r and r.body
+            -- TODO: handle error or response nil here, e.g. logging
+            -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+            local response_data = { { id, response } }
+
+            ---@diagnostic disable-next-line: param-type-mismatch
+            client:notify("tsserver/response", response_data)
+          end)
+        end
+      end,
+    }
+
     opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
       ts_ls = ts_ls_config,
-      vue_ls = {},
+      vue_ls = vue_ls_config,
 
       intelephense = {
         settings = {
@@ -41,9 +80,9 @@ return {
       },
     }
 
-    -- Ensure both servers are installed
+    -- Ensure servers are installed
     opts.mason_lspconfig = vim.tbl_deep_extend("force", opts.mason_lspconfig or {}, {
-      ensure_installed = { "vue_ls", "vtsls", "intelephense" },
+      ensure_installed = { "vue_ls", "ts_ls", "intelephense" },
     })
 
     return opts
