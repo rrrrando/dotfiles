5a196e71b9afc9b9587b707fa68ac50d09128792
Rando Õispuu <rando@veebisepad.ee>
2025-10-30 10:40:09 +0200

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -1,7 +1,6 @@
 return {
   "AstroNvim/astrolsp",
   opts = function(_, opts)
-    -- Get the path to the vue language server plugin
     local vue_language_server_path = vim.fn.stdpath("data")
         .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
     local vue_plugin = {
@@ -10,44 +9,20 @@ return {
       languages = { "vue" },
       configNamespace = "typescript",
     }
-
-    -- vtsls config as per vuejs wiki
-    opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
-      vtsls = {
-        settings = {
-          vtsls = {
-            tsserver = {
-              globalPlugins = { vue_plugin },
-            },
-          },
+    local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+    local ts_ls_config = {
+      init_options = {
+        plugins = {
+          vue_plugin,
         },
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
       },
-      volar = {
-        on_init = function(client)
-          client.handlers["tsserver/request"] = function(_, result, context)
-            local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-            if #clients == 0 then
-              vim.notify(
-                "Could not find `vtsls` lsp client, `vue_ls` would not work without it.",
-                vim.log.levels.ERROR
-              )
-              return
-            end
-            local ts_client = clients[1]
-            local param = unpack(result)
-            local id, command, payload = unpack(param)
-            ts_client:exec_cmd({
-              title = "vue_request_forward",
-              command = "typescript.tsserverRequest",
-              arguments = { command, payload },
-            }, { bufnr = context.bufnr }, function(_, r)
-              local response_data = { { id, r.body } }
-              client:notify("tsserver/response", response_data)
-            end)
-          end
-        end,
-      },
+      filetypes = tsserver_filetypes,
+    }
+
+    opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
+      ts_ls = ts_ls_config,
+      vue_ls = {},
+
       intelephense = {
         settings = {
           intelephense = {
