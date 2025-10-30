e8c52d471adad444b2ab814db9c348ccf010dcaa
Rando Õispuu <rando@veebisepad.ee>
2025-10-30 11:14:25 +0200

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -1,90 +1,91 @@
 return {
-  "AstroNvim/astrolsp",
-  opts = function(_, opts)
-    local vue_language_server_path = vim.fn.stdpath("data")
-        .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-    local vue_plugin = {
-      name = "@vue/typescript-plugin",
-      location = vue_language_server_path,
-      languages = { "vue" },
-      configNamespace = "typescript",
-    }
-    local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-    local ts_ls_config = {
-      init_options = {
-        plugins = {
-          vue_plugin,
-        },
-      },
-      filetypes = tsserver_filetypes,
-    }
+	"AstroNvim/astrolsp",
+	opts = function(_, opts)
+		local vue_language_server_path = vim.fn.stdpath("data")
+			.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+		local vue_plugin = {
+			name = "@vue/typescript-plugin",
+			location = vue_language_server_path,
+			languages = { "vue" },
+			configNamespace = "typescript",
+		}
+		local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+		local vtsls_config = {
+			settings = {
+				vtsls = {
+					tsserver = {
+						globalPlugins = {
+							vue_plugin,
+						},
+					},
+				},
+			},
+			filetypes = tsserver_filetypes,
+		}
 
-    local vue_ls_config = {
-      on_init = function(client)
-        client.handlers["tsserver/request"] = function(_, result, context)
-          local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "ts_ls" })
-          local clients = {}
+		local vue_ls_config = {
+			on_init = function(client)
+				client.handlers["tsserver/request"] = function(_, result, context)
+					local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
 
-          vim.list_extend(clients, ts_clients)
+					if #clients == 0 then
+						vim.notify(
+							"Could not find `vtsls` lsp client, `vue_ls` would not work without it.",
+							vim.log.levels.ERROR
+						)
+						return
+					end
+					local ts_client = clients[1]
 
-          if #clients == 0 then
-            vim.notify(
-              "Could not find `ts_ls` lsp client, `vue_ls` would not work without it.",
-              vim.log.levels.ERROR
-            )
-            return
-          end
-          local ts_client = clients[1]
+					local param = unpack(result)
+					local id, command, payload = unpack(param)
+					ts_client:exec_cmd({
+						title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+						command = "typescript.tsserverRequest",
+						arguments = {
+							command,
+							payload,
+						},
+					}, { bufnr = context.bufnr }, function(_, r)
+						local response = r and r.body
+						-- TODO: handle error or response nil here, e.g. logging
+						-- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+						local response_data = { { id, response } }
 
-          local param = unpack(result)
-          local id, command, payload = unpack(param)
-          ts_client:exec_cmd({
-            title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-            command = "typescript.tsserverRequest",
-            arguments = {
-              command,
-              payload,
-            },
-          }, { bufnr = context.bufnr }, function(_, r)
-            local response = r and r.body
-            -- TODO: handle error or response nil here, e.g. logging
-            -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
-            local response_data = { { id, response } }
+						---@diagnostic disable-next-line: param-type-mismatch
+						client:notify("tsserver/response", response_data)
+					end)
+				end
+			end,
+		}
 
-            ---@diagnostic disable-next-line: param-type-mismatch
-            client:notify("tsserver/response", response_data)
-          end)
-        end
-      end,
-    }
+		opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
+			vtsls = vtsls_config,
+			volar = vue_ls_config,
 
-    opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
-      ts_ls = ts_ls_config,
-      volar = vue_ls_config,
+			intelephense = {
+				settings = {
+					intelephense = {
+						single_file_support = true,
+						completion = {
+							maxItems = 200,
+						},
+					},
+				},
+			},
+		})
 
-      intelephense = {
-        settings = {
-          intelephense = {
-            single_file_support = true,
-            completion = {
-              maxItems = 200,
-            },
-          },
-        },
-      },
-    })
+		opts.formatting = {
+			disabled = {
+				"intelephense",
+			},
+		}
 
-    opts.formatting = {
-      disabled = {
-        "intelephense",
-      },
-    }
+		-- Ensure servers are installed
+		opts.mason_lspconfig = vim.tbl_deep_extend("force", opts.mason_lspconfig or {}, {
+			ensure_installed = { "vue_ls", "vtsls", "intelephense" },
+		})
 
-    -- Ensure servers are installed
-    opts.mason_lspconfig = vim.tbl_deep_extend("force", opts.mason_lspconfig or {}, {
-      ensure_installed = { "vue_ls", "ts_ls", "intelephense" },
-    })
-
-    return opts
-  end,
+		return opts
+	end,
 }
