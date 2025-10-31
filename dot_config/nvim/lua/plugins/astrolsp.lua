c773ebbba0ab4a0471efedfddffbb2c1d62bf6e0
Rando Õispuu <rando@veebisepad.ee>
2025-10-31 22:02:07 +0200

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -2,7 +2,7 @@ return {
 	"AstroNvim/astrolsp",
 	opts = function(_, opts)
 		local vue_language_server_path = vim.fn.stdpath("data")
-			.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+				.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 		local vue_plugin = {
 			name = "@vue/typescript-plugin",
 			location = vue_language_server_path,
@@ -23,6 +23,26 @@ return {
 						globalPlugins = {
 							vue_plugin,
 						},
+					},
+				},
+				typescript = {
+					suggest = {
+						includeCompletionsForModuleExports = true,
+						includeAutomaticOptionalChainCompletions = true,
+					},
+					preferences = {
+						includePackageJsonAutoImports = "on",
+						importModuleSpecifierPreference = "relative",
+					},
+				},
+				javascript = {
+					suggest = {
+						includeCompletionsForModuleExports = true,
+						includeAutomaticOptionalChainCompletions = true,
+					},
+					preferences = {
+						includePackageJsonAutoImports = "on",
+						importModuleSpecifierPreference = "relative",
 					},
 				},
 			},
