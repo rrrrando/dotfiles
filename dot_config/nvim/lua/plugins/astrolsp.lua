cac393e651c17eafcf5d2bbf5e2447b4e8175de2
Rando Õispuu <rando@veebisepad.ee>
2025-10-31 23:13:09 +0200

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -25,37 +25,40 @@ return {
 		}
 
 		local vue_ls_config = {
-			on_init = function(client)
-				client.handlers["tsserver/request"] = function(_, result, context)
-					local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-
-					if #clients == 0 then
-						vim.notify(
-							"Could not find `vtsls` lsp client, `vue_ls` would not work without it.",
-							vim.log.levels.ERROR
-						)
-						return
-					end
-					local ts_client = clients[1]
-
-					local param = unpack(result)
-					local id, command, payload = unpack(param)
-					ts_client:exec_cmd({
-						title = "vue_request_forward",
-						command = "typescript.tsserverRequest",
-						arguments = {
-							command,
-							payload,
-						},
-					}, { bufnr = context.bufnr }, function(_, r)
-						local response = r and r.body
-						local response_data = { { id, response } }
-
-						---@diagnostic disable-next-line: param-type-mismatch
-						client:notify("tsserver/response", response_data)
-					end)
-				end
-			end,
+			vue = {
+				hybridMode = false,
+			},
+			-- on_init = function(client)
+			-- 	client.handlers["tsserver/request"] = function(_, result, context)
+			-- 		local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+			--
+			-- 		if #clients == 0 then
+			-- 			vim.notify(
+			-- 				"Could not find `vtsls` lsp client, `vue_ls` would not work without it.",
+			-- 				vim.log.levels.ERROR
+			-- 			)
+			-- 			return
+			-- 		end
+			-- 		local ts_client = clients[1]
+			--
+			-- 		local param = unpack(result)
+			-- 		local id, command, payload = unpack(param)
+			-- 		ts_client:exec_cmd({
+			-- 			title = "vue_request_forward",
+			-- 			command = "typescript.tsserverRequest",
+			-- 			arguments = {
+			-- 				command,
+			-- 				payload,
+			-- 			},
+			-- 		}, { bufnr = context.bufnr }, function(_, r)
+			-- 			local response = r and r.body
+			-- 			local response_data = { { id, response } }
+			--
+			-- 			---@diagnostic disable-next-line: param-type-mismatch
+			-- 			client:notify("tsserver/response", response_data)
+			-- 		end)
+			-- 	end
+			-- end,
 		}
 
 		opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
