87d9774d43c2e52687c876055ba75e29cd18aa58
Rando Õispuu <rando@veebisepad.ee>
2025-11-02 21:19:24 +0200

    Update .config/nvim/lua/plugins/user.lua

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -1,38 +1,39 @@
 ---@type LazySpec
 return {
-  -- customize dashboard options
-  {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-      picker = {
-        matcher = {
-          frecency = true,
-        },
-        sort = {
-          fields = { "score:desc", "#text", "idx" },
-        },
-      },
-      scroll = {},
-      terminal = {},
-      dashboard = {
-        preset = {
-          header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
-          }, "\n"),
-        },
-      },
-    },
-  },
-  { "wakatime/vim-wakatime", lazy = false },
+	-- customize dashboard options
+	{
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			gh = {},
+			picker = {
+				matcher = {
+					frecency = true,
+				},
+				sort = {
+					fields = { "score:desc", "#text", "idx" },
+				},
+			},
+			scroll = {},
+			terminal = {},
+			dashboard = {
+				preset = {
+					header = table.concat({
+						" █████  ███████ ████████ ██████   ██████ ",
+						"██   ██ ██         ██    ██   ██ ██    ██",
+						"███████ ███████    ██    ██████  ██    ██",
+						"██   ██      ██    ██    ██   ██ ██    ██",
+						"██   ██ ███████    ██    ██   ██  ██████ ",
+						"",
+						"███    ██ ██    ██ ██ ███    ███",
+						"████   ██ ██    ██ ██ ████  ████",
+						"██ ██  ██ ██    ██ ██ ██ ████ ██",
+						"██  ██ ██  ██  ██  ██ ██  ██  ██",
+						"██   ████   ████   ██ ██      ██",
+					}, "\n"),
+				},
+			},
+		},
+	},
+	{ "wakatime/vim-wakatime", lazy = false },
 }
