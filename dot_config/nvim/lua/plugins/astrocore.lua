fcc090d3d2cbf6708db41eae646351e13eae712c
Rando Õispuu <rando@veebisepad.ee>
2025-11-02 21:25:26 +0200

    Update .config/nvim/lua/plugins/astrocore.lua

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -10,12 +10,12 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
+      large_buf = { size = 1024 * 256, lines = 10000 },          -- set global limits for large files for disabling features like treesitter
+      autopairs = true,                                          -- enable autopairs at start
+      cmp = true,                                                -- enable completion at start
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      highlighturl = true,                                       -- highlight URLs at start
+      notifications = true,                                      -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -37,12 +37,12 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {              -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        number = true,     -- sets vim.opt.number
+        spell = false,     -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = false,      -- sets vim.opt.wrap
         fileformats = "unix,dos,mac",
         fixendofline = false,
       },
@@ -60,17 +60,52 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["]b"] = {
+          function()
+            require("astrocore.buffer").nav(vim.v.count1)
+          end,
+          desc = "Next buffer",
+        },
+        ["[b"] = {
+          function()
+            require("astrocore.buffer").nav(-vim.v.count1)
+          end,
+          desc = "Previous buffer",
+        },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
           function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
+            require("astroui.status.heirline").buffer_picker(function(bufnr)
+              require("astrocore.buffer").close(bufnr)
+            end)
           end,
           desc = "Close buffer from tabline",
+        },
+
+        ["<leader>gi"] = {
+          function()
+            Snacks.picker.gh_issue()
+          end,
+          desc = "GitHub Issues (open)",
+        },
+        ["<leader>gI"] = {
+          function()
+            Snacks.picker.gh_issue({ state = "all" })
+          end,
+          desc = "GitHub Issues (all)",
+        },
+        ["<leader>gp"] = {
+          function()
+            Snacks.picker.gh_pr()
+          end,
+          desc = "GitHub Pull Requests (open)",
+        },
+        ["<leader>gP"] = {
+          function()
+            Snacks.picker.gh_pr({ state = "all" })
+          end,
+          desc = "GitHub Pull Requests (all)",
         },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
@@ -91,7 +126,7 @@ return {
         -- ["<C-S>"] = false,
       },
       i = {
-        ["<C-s>"] = { "<esc>:w!<cr>", desc = "Save File" },
+        ["<C-s>"] = { "<esc>:wa!<cr>", desc = "Save all Files" },
       },
     },
   },
