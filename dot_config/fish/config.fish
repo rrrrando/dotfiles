967ed048ac67043cc2eccbac951e7caf0fb092d8
Rando Oispuu <rando@innofly.eu>
2025-11-09 19:48:53 +0200

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,8 +9,6 @@ bind \cr _atuin_search
 #ENV VARS
 set -Ux HERD_PHP_83_INI_SCAN_DIR "$HOME/Library/Application Support/Herd/config/php/83/"
 set -Ux HERD_PHP_84_INI_SCAN_DIR "$HOME/Library/Application Support/Herd/config/php/84/"
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
 set -gx EDITOR "nvim"
 set -gx HOMEBREW_NO_ENV_HINTS 1
 set -gx XDG_CONFIG_HOME "$HOME/.config"
