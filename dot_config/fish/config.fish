abbd9d543acd081a4b1ebe08a68429fd8e9d53ba
Rando Oispuu <rando@innofly.eu>
2025-11-09 15:16:15 +0200

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,7 +2,7 @@ set fish_greeting
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+# source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 set -gx ATUIN_NOBIND "true"
 atuin init fish | source
