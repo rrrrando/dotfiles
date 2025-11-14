2cacd880c041d7a50b85167b98739fdb516075c1
Rando Õispuu <rando@veebisepad.ee>
2025-11-14 22:49:30 +0200

    Update .config/nvim/lua/plugins/astrocore.lua

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -126,6 +126,8 @@ return {
 					noremap = false,
 					expr = true,
 				},
+				["<Leader>."] = {function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" }},
+				["<Leader>bS"] = {function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" }},
 				-- setting a mapping to false will disable it
 				-- ["<C-S>"] = false,
 			},
